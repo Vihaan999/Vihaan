010c6a4720f9214c43f746a43a0a0df8511074a4
Vihaan999 <Vihaanmagan@icloud.com>
2024-07-09 13:35:20 -0500

    Add files via upload

---
+++ Tic Tac Toe/TicTacToe.py
@@ -0,0 +1,55 @@
+def fullprint(gameboard):
+    for i in range(0,len(gameboard)):
+        for k in range(0,len(gameboard[i])):
+            if k == len(gameboard[i])-1:
+                print(gameboard[i][k])
+            else: 
+                print(gameboard[i][k], end=" ")
+#~~~Variables~~~#
+player="X"
+
+gameboard=[
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+    ]
+
+endTheGame=False
+
+#~~~Playing the Game~~~#
+print("welcome to Tic-Tac-Toe")
+while(endTheGame == False):
+    if player=="X":
+        player="O"
+    elif player=="O":
+        player="X"
+    fullprint(gameboard)
+    Y= int(input("Which row? (1-3)"))-1
+        
+    X= int(input("Which column? (1-3)"))-1
+    gameboard[Y][X]=player
+
+    if gameboard[0][0]==gameboard[0][1]==gameboard[0][2] != 0:
+        print ("You win!!!:)")
+    if gameboard[1][0]==gameboard[1][1]==gameboard[1][2] != 0:
+        print ("You win!!!:)")
+    if gameboard[2][0]==gameboard[2][1]==gameboard[2][2] != 0:
+        print ("You win!!!:)")
+
+
+    if gameboard[0][0]==gameboard[1][0]==gameboard[2][0] != 0:
+        print ("You win!!!:)")
+    
+    if gameboard[0][1]==gameboard[1][1]==gameboard[2][1] != 0:
+        print ("You win!!!:)")
+    
+    if gameboard[0][2]==gameboard[1][2]==gameboard[2][2] != 0:
+        print ("You win!!!:)")
+    
+
+    if gameboard[0][0]==gameboard[1][1]==gameboard[2][2] != 0:
+        print ("You win!!!:)")
+    if gameboard[0][2]==gameboard[1][1]==gameboard[2][0] != 0:
+        print ("You win!!! :)")
+    
+    
